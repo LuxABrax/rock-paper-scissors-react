b06366669bbb2241c67b7c3691944ca462fee32f
LuxABrax <lukabr394x@gmail.com>
2020-09-17 23:31:40 +0200

    added stuff

--- rock-paper-scissors-frontend/src/components/GameScreen.js
+++ rock-paper-scissors-frontend/src/components/GameScreen.js
@@ -4,6 +4,7 @@ const GameScreen = () => {
 	return (
 		<div className='gameContainer'>
 			<h3>Game screen</h3>
+			<p>Countdown</p>
 		</div>
 	);
 };
--- rock-paper-scissors-frontend/src/pages/RoomPage.js
+++ rock-paper-scissors-frontend/src/pages/RoomPage.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import io from "socket.io-client";
+import queryStream from "query-stream";
 import GameScreen from "../components/GameScreen";
 import HandOptions from "../components/HandOptions";
 import { Link } from "react-router-dom";
 
 const RoomPage = () => {
+	useEffect(() => {
+		const { room, username } = queryStream.parse(location.search);
+	}, []);
 	return (
 		<div className='roomContainer'>
 			<Link to='/'>Home</Link>
+			<HandOptions />
 			<GameScreen />
 			<HandOptions />
 		</div>
