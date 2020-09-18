4ea1f760f35fc6adedeb2c579830d7ec0c432b9d
Lux <lukabr394x@gmail.com>
2020-09-18 03:15:00 +0200

    added timer

--- rock-paper-scissors-frontend/src/pages/RoomPage.js
+++ rock-paper-scissors-frontend/src/pages/RoomPage.js
@@ -8,31 +8,39 @@ import { Link } from "react-router-dom";
 let socket;
 
 const RoomPage = ({ location }) => {
-	const ENDPOINT = 'localhost:5002';
-	const [room, setRoom] = useState({ roomName: '', players: [] })
+	const ENDPOINT = "localhost:5002";
+	const [room, setRoom] = useState({ roomName: "", players: [] });
 
 	useEffect(() => {
 		const { room, username } = queryString.parse(location.search);
-		console.log(room, username)
+		console.log(room, username);
 		socket = io(ENDPOINT);
 
 		setRoom({ roomName: room, players: [username] });
 
-		socket.emit('joinRoom', { roomName: room, username }, () => {
+		socket.emit("joinRoom", { roomName: room, username }, () => {
 			console.log(room, username);
 		});
 
 		return () => {
-			socket.emit('disconnect');
+			socket.emit("disconnect");
 			socket.off();
-		}
-
+		};
 	}, [ENDPOINT, location.seach]);
+
+	//! Timer !
+	const [counter, setCounter] = useState(10);
+	useEffect(() => {
+		const timer =
+			counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+		return () => clearInterval(timer);
+	}, [counter]);
 
 	return (
 		<div className='roomContainer'>
 			<Link to='/'>Home</Link>
 			<HandOptions />
+			<h1 className='timer'>{counter}</h1>
 			<GameScreen />
 			<HandOptions />
 		</div>
