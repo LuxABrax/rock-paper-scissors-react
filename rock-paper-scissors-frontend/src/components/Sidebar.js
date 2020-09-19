be2be3bd26e2a0029b01696526dd1324c5701522
Lux <lukabr394x@gmail.com>
2020-09-19 04:23:37 +0200

    aa

---
+++ rock-paper-scissors-frontend/src/components/Sidebar.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Sidebar.css";
+const Sidebar = ({ joinRoom }) => {
+	const [username, setName] = useState("");
+	const [room, setRoom] = useState("");
+	const joinRoom = e => {
+		e.preventDefault();
+	};
+	return (
+		<div className='sidebar'>
+			<form className='join-form'>
+				<label htmlFor='username'>Username</label>
+				<input
+					className='join-input'
+					id='username'
+					type='text'
+					value={username}
+					onChange={e => setName(e.target.value)}
+				/>
+				<label htmlFor='room'>Room</label>
+				<input
+					className='join-input'
+					id='room'
+					type='text'
+					value={room}
+					onChange={e => setRoom(e.target.value)}
+				/>
+				<button
+					className='join-btn'
+					type='submit'
+					onClick={e => joinRoom(username, room)}
+				>
+					Join
+				</button>
+			</form>
+		</div>
+	);
+};
+
+export default Sidebar;
--- rock-paper-scissors-frontend/src/pages/RoomPage.js
+++ rock-paper-scissors-frontend/src/pages/RoomPage.js
@@ -5,30 +5,29 @@ import GameScreen from "../components/GameScreen";
 import HandOptions from "../components/HandOptions";
 import { Link } from "react-router-dom";
 import "./RoomPage.css";
+import Sidebar from "../components/Sidebar";
 
 let socket;
 
 const RoomPage = ({ location }) => {
 	const ENDPOINT = "localhost:5002";
 	const [room, setRoom] = useState({ roomName: "", players: [] });
-
+	const [user, setUser] = useState({ roomName: "", userName: "" });
 	useEffect(() => {
-		const { room, username } = queryString.parse(location.search);
-		console.log(room, username);
 		socket = io(ENDPOINT);
-
-		setRoom({ roomName: room, players: [username] });
-
-		socket.emit("joinRoom", { roomName: room, username }, () => {
-			console.log(room, username);
-		});
 
 		return () => {
 			socket.emit("disconnect");
 			socket.off();
 		};
-	}, [ENDPOINT, location.seach]);
+	}, [ENDPOINT]);
 
+	const joinRoom = (username, roomName) => {
+		setUser({ roomName, username });
+		socket.emit("joinRoom", { roomName, username }, () => {
+			console.log(room, username);
+		});
+	};
 	//! Timer !
 
 	const [counter, setCounter] = useState(0);
@@ -40,11 +39,10 @@ const RoomPage = ({ location }) => {
 	}, [counter]);
 
 	return (
-		<div className='roomContainer'>
+		<div className='main'>
 			<Link to='/'>
 				<button className='home-btn'>Home</button>
 			</Link>
-			<HandOptions player={2} />
 			<div className='cTimer'>
 				<button
 					onClick={() => {
@@ -55,8 +53,16 @@ const RoomPage = ({ location }) => {
 				</button>
 				<h1 className='timer'>{counter}</h1>
 			</div>
-			<GameScreen />
-			<HandOptions player={1} />
+			<div className='roomContainer'>
+				<div className='sidebar-container'>
+					<Sidebar value={joinRoom} />
+				</div>
+				<div className='room-board'>
+					<HandOptions player={2} />
+					<GameScreen />
+					<HandOptions player={1} />
+				</div>
+			</div>
 		</div>
 	);
 };
