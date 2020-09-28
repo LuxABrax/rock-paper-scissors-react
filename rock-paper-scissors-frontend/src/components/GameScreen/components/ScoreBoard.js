842a06c78634187528240f4407f1b0a9b7752333
Lux <lukabr394x@gmail.com>
2020-09-28 21:55:15 +0200

    scoreboard and roomname fixes

--- rock-paper-scissors-frontend/src/components/GameScreen/components/ScoreBoard.js
+++ rock-paper-scissors-frontend/src/components/GameScreen/components/ScoreBoard.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-import './ScoreBoard.css';
+import "./ScoreBoard.css";
 
-export default function ScoreBoard({ user, results, socketID }) {
+export default function ScoreBoard({
+	scoreB,
+	setScoreB,
+	user,
+	results,
+	socketID,
+}) {
+	useEffect(() => {
+		let result;
+		if (results) {
+			if (socketID === results[0].socketID) {
+				result = results[0].result;
+			} else if (socketID === results[1].socketID) {
+				result = results[1].result;
+			}
 
-    const [score, setScore] = useState({ user: 0, opponent: 0 });
+			if (result === "win") {
+				let points = scoreB.user + 1;
+				console.log("I won", points);
+				setScoreB({ ...scoreB, user: points });
+			} else if (result === "draw") {
+				let points = scoreB.draw + 1;
+				setScoreB({ ...scoreB, draw: points });
+			} else {
+				let points = scoreB.opponent + 1;
+				console.log("I lost", points);
+				setScoreB({ ...scoreB, opponent: points });
+			}
+		}
+	}, [results]);
 
-    useEffect(() => {
-        let result;
-        if (results) {
-            if (socketID === results[0].socketID) {
-                result = results[0].result;
-            } else if (socketID === results[1].socketID) {
-                result = results[1].result;
-            }
-
-            if (result === 'win') {
-                console.log("I won");
-                let points = score.user + 1;
-                console.log("I lost", points);
-                setScore({ ...score, user: points });
-
-            } else {
-                let points = score.opponent + 1;
-                console.log("I lost", points);
-                setScore({ ...score, opponent: points });
-            }
-
-        }
-
-    }, [results]);
-
-    return (
-
-        <div className='score-container'>
-
-            {score.user} : {score.opponent}
-        </div>
-    )
+	return (
+		<div className='score-container'>
+			<div className='score sw'>{scoreB.user}</div>
+			<div className='score sd'>{scoreB.draw}</div>
+			<div className='score sl'>{scoreB.opponent}</div>
+		</div>
+	);
 }
--- rock-paper-scissors-frontend/src/pages/RoomPage.js
+++ rock-paper-scissors-frontend/src/pages/RoomPage.js
@@ -6,7 +6,6 @@ import { Link } from "react-router-dom";
 import "./RoomPage.css";
 import Sidebar from "../components/Sidebar";
 
-
 let socket;
 
 const RoomPage = ({ location }) => {
@@ -17,16 +16,14 @@ const RoomPage = ({ location }) => {
 	//const [active, setActive] = useState("none");
 	const [mode, setMode] = useState("prep");
 	const [gameReady, setGameReady] = useState(false);
+	const [roomState, setRoomState] = useState("none");
 
 	const [res, setRes] = useState(undefined);
 	const [score, setScore] = useState({
 		mywins: 0,
 		opWins: 0,
 	});
-
-
-
-
+	const [scoreB, setScoreB] = useState({ user: 0, opponent: 0, draw: 0 });
 	useEffect(() => {
 		socket = io(ENDPOINT);
 
@@ -37,6 +34,7 @@ const RoomPage = ({ location }) => {
 
 		socket.on("userJoined", ({ username, roomName, players }) => {
 			setUsers([...players]);
+			setRoomState(roomName);
 			console.log(users, username, roomName);
 		});
 		socket.on("gameReady", () => {
@@ -44,16 +42,16 @@ const RoomPage = ({ location }) => {
 			setMode("time");
 		});
 
-		socket.on("results", (results) => {
+		socket.on("results", results => {
 			let result;
 			setRes(results);
-			console.log('halobaba')
+			console.log("halobaba");
 			if (socket.id === results[0].socketID) {
 				result = results[0].result;
 			} else if (socket.id === results[1].socketID) {
 				result = results[1].result;
 			}
-			if (result === 'win') {
+			if (result === "win") {
 				console.log("I won");
 				//setPoints([points[0] + 1, points[1]]);
 			} else {
@@ -62,11 +60,10 @@ const RoomPage = ({ location }) => {
 			}
 			setScore({
 				...score,
-				result
+				result,
 			});
 
 			setMode("result");
-
 		});
 
 		return () => {
@@ -93,9 +90,7 @@ const RoomPage = ({ location }) => {
 		socket.emit("userReady", user.roomName);
 	};
 	const sendResult = result => {
-
-		if (user.username && user.roomName && mode === 'fight') {
-
+		if (user.username && user.roomName && mode === "fight") {
 			socket.emit("result", {
 				result,
 				username: user.username,
@@ -104,48 +99,23 @@ const RoomPage = ({ location }) => {
 		}
 	};
 
-	/* 	const calcResult = (userInput, opponentInput) => {
-			let result = { userWon: false, opponentWon: false };
-			switch (userInput) {
-				case 'rock':
-					if (opponentInput === 'paper') result.oponentWon = true;
-					if (opponentInput === 'scissor') result.userWon = true;
-					break;
-				case 'paper':
-					if (opponentInput === 'rock') result.userWon = true;
-					if (opponentInput === 'scissor') result.opponentWon = true;
-					break;
-				case 'scissor':
-					if (opponentInput === 'rock') result.opponentWon = true;
-					if (opponentInput === 'paper') result.userWon = true;
-					break;
-				default:
-			}
-	
-			return result;
-	
-		} */
-	//! Timer !
-
 	return (
 		<div className='main'>
-			<button className='room-btn' onClick={() => selectRoom()}>
-				Room
-			</button>
+			<div className='room-btn' onClick={() => selectRoom()}>
+				<h3>Room : {roomState}</h3>
+			</div>
 
 			<div className='roomContainer'>
 				<div
-					className={`${sideBar ? "sidebar-open" : "sidebar-container"
-						}`}
+					className={`${
+						sideBar ? "sidebar-open" : "sidebar-container"
+					}`}
 				>
 					<Sidebar joinRoom={joinRoom} />
 				</div>
 				{user ? (
 					<div className='room-board'>
-						<HandOptions
-							player={2}
-							sendResult={sendResult}
-						/>
+						<HandOptions player={2} sendResult={sendResult} />
 						<GameScreen
 							mode={mode}
 							setMode={setMode}
@@ -155,21 +125,20 @@ const RoomPage = ({ location }) => {
 							score={score}
 							results={res}
 							socketID={socket.id}
+							scoreB={scoreB}
+							setScoreB={setScoreB}
 						/>
 						<HandOptions
 							player={1}
 							sendResult={sendResult}
+							mode={mode}
 						/>
 					</div>
 				) : (
-						<h1 className='room-board enter-room'>Enter Room!</h1>
-					)}
-				{/* <div className='room-board'>
-					<HandOptions player={2} sendResult={sendResult} />
-					<GameScreen />
-					<HandOptions player={1} sendResult={sendResult} />
-				</div> */}
-				<div className='sidebar-info-container'>
+					<h1 className='room-board enter-room'>Enter Room!</h1>
+				)}
+
+				{/* <div className='sidebar-info-container'>
 					<div className='roomInformation'>
 						<h2 className='roomName__info'>
 							Room: {user ? user.roomName : ""}
@@ -178,7 +147,7 @@ const RoomPage = ({ location }) => {
 							Players: {users ? users.length : users}
 						</h3>
 					</div>
-				</div>
+				</div> */}
 			</div>
 		</div>
 	);
