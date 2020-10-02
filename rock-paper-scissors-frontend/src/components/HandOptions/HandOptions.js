9e1da6eeb1043d2d61745e301efb7e2aee6c90be
Robin Ribaric <robin.ribaric@appliedtechnology.se>
2020-10-02 15:33:27 +0200

    created hadn component

--- rock-paper-scissors-frontend/src/components/HandOptions/HandOptions.js
+++ rock-paper-scissors-frontend/src/components/HandOptions/HandOptions.js
@@ -1,19 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "./HandOptions.css";
+
+import Hand from './Hand/Hand';
+
+
+/* 
+MAKE BACKGROUND AND CREATE HAND COMPONENT
+*/
+
 const HandOptions = ({ player, sendResult, theme, mode }) => {
-	const [active, setActive] = useState("none");
-	const [clickable, setClickable] = useState(true);
-	const [handTheme, setHandTheme] = useState({
-		rock: "/images/rock-classic.jpg",
-		paper: "/images/paper-classic.jpg",
-		scis: "/images/scis-classic.jpg",
-	});
+	/* const [active, setActive] = useState("none");
+	const [clickable, setClickable] = useState(true); */
+	//const [signs, setSigns] = useState(['paper', 'scissors', 'rock']);
+	const [signs, setSigns] = useState([
+		{ sign: 'rock', img: "/images/rock-classic.jpg" },
+		{ sign: 'paper', img: "/images/paper-classic.jpg" },
+		{ sign: 'scissors', img: "/images/scis-classic.jpg" },
+	]);
+
+	/* 	const [themes, setThemes] = useState({
+			classic: [
+				rock: "/images/rock-classic.jpg",
+				paper: "/images/paper-classic.jpg",
+				scis: "/images/scis-classic.jpg",
+			],
+			poop: [
+				rock: "/images/rock-poop.jpg",
+				paper: "/images/paper-poop.jpg",
+				scis: "/images/scis-poop.jpg",
+			],
+			batman: [
+				"/images/rock-batman.jpg",
+				"/images/paper-batman.jpg",
+				"/images/scis-batman.jpg",
+			]
+		});
+	
+		const [handTheme, setHandTheme] = useState({
+			rock: "/images/rock-classic.jpg",
+			paper: "/images/paper-classic.jpg",
+			scis: "/images/scis-classic.jpg",
+		}); */
 
 	/* makes components clickable again when result is shown, the sendResult function in roompage can
 	| can only send whend mode === fight, so no need yo check it here
 	*/
 
-	useEffect(() => {
+	/* useEffect(() => {
 		if (mode === "result") setClickable(!clickable);
 	}, [mode]);
 	useEffect(() => {
@@ -40,13 +73,18 @@ const HandOptions = ({ player, sendResult, theme, mode }) => {
 				});
 				break;
 		}
-	}, [theme]);
+	}, [theme]); */
+
+
 	return (
 		<div className={`HandOptions player${player}`}>
-			<div
-				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${
-					active === "rock" ? "active" : ""
-				}`}
+
+			{signs.map(sign => <Hand sendResult={sendResult} sign={sign.sign} img={sign.img} mode={mode} />)}
+
+
+			{/* <div
+				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${active === "rock" ? "active" : ""
+					}`}
 				onClick={() => {
 					if (mode === "fight") {
 						setActive("rock");
@@ -58,9 +96,8 @@ const HandOptions = ({ player, sendResult, theme, mode }) => {
 				<img src={handTheme.rock} alt='rock' />
 			</div>
 			<div
-				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${
-					active === "paper" ? "active" : ""
-				}`}
+				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${active === "paper" ? "active" : ""
+					}`}
 				onClick={() => {
 					if (mode === "fight") {
 						setActive("paper");
@@ -72,9 +109,8 @@ const HandOptions = ({ player, sendResult, theme, mode }) => {
 				<img src={handTheme.paper} alt='paper' />
 			</div>
 			<div
-				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${
-					active === "scissors" ? "active" : ""
-				}`}
+				className={`Hand pos1 ${clickable ? "" : "signNotClickable"} ${active === "scissors" ? "active" : ""
+					}`}
 				onClick={() => {
 					if (mode === "fight") {
 						setActive("scissors");
@@ -84,7 +120,7 @@ const HandOptions = ({ player, sendResult, theme, mode }) => {
 				}}
 			>
 				<img src={handTheme.scis} alt='scissors' />
-			</div>
+			</div> */}
 		</div>
 	);
 };
